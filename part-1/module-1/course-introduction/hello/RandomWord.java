efdd6514e0a6673f2df508ed3d0604a96d472942
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-04-22 19:06:28 -0400

    Working on figuring out differentiating typed input from text file input

---
+++ part-1/module-1/course-introduction/hello/RandomWord.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        while (StdIn.hasNextChar()) {
+            StdOut.println(StdIn.readString());
+        }
+        // System.out.println(StdIn.readAll());
+        // while (!StdIn.isEmpty()) {
+        // System.out.println(StdIn.readString());
+        // System.out.println(StdIn.isEmpty());
+
+        // }
+    }
+}
