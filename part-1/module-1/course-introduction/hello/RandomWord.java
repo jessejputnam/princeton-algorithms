6f94fa1ad54faa3d80c151ae964c1608eaf9b50b
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-04-23 09:20:48 -0400

    Solve RandomWord for file input, but still issues with console input

--- part-1/module-1/course-introduction/hello/RandomWord.java
+++ part-1/module-1/course-introduction/hello/RandomWord.java
@@ -4,13 +4,48 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
-        while (StdIn.hasNextChar()) {
-            StdOut.println(StdIn.readString());
+        boolean isFile = System.console() == null;
+        String champion = "";
+
+        if (!isFile) {
+            String line = StdIn.readLine();
+
+            for (int i = 1; !StdIn.isEmpty(); i++) {
+                String word = StdIn.readString();
+                System.out.println(i + ": " + word);
+
+            }
+            // int wordNum = 0;
+            // String line = StdIn.readLine();
+
+            // StdOut.println(StdIn.readString());
+        } else {
+            for (int i = 1; !StdIn.isEmpty(); i++) {
+                boolean chance = StdRandom.bernoulli(1.0 / i);
+                String contender = StdIn.readString();
+                if (chance) {
+                    champion = contender;
+                }
+            }
         }
-        // System.out.println(StdIn.readAll());
-        // while (!StdIn.isEmpty()) {
-        // System.out.println(StdIn.readString());
-        // System.out.println(StdIn.isEmpty());
+
+        System.out.println(champion);
+
+        // for (int i = 1; !StdIn.isEmpty(); i++) {
+        // // System.out.println(StdIn.readString());
+        // boolean check = StdRandom.bernoulli(1.0 / i);
+        // String word = StdIn.readString();
+        // System.out.println(check);
+        // if (check) {
+        // System.out.println(word);
+        // break;
+        // }
+        // }
+
+        // boolean nextLine = StdIn.hasNextLine();
+        // while (nextLine) {
+        // System.out.println("String: " + StdIn.readString());
+        // System.out.println("Empty: " + StdIn.isEmpty());
 
         // }
     }
